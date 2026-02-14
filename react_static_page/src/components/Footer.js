0bba0642a72d6a22c1a66f43e665d5c9d3970ffa
jeremyauguste <jeremy.augu@gmail.com>
2026-02-13 23:43:36 -0500

    added page content

---
+++ react_static_page/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <h2>The Moral of the Story:</h2>
+            <p>Love doesn't focus on form, but it trancends it and all obstacles.</p>
+        </div>
+    )
+}
+
+export default Footer
