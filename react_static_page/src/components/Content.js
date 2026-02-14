e1c8a8e0507cbb516d1eb96649060750c0c14d5e
jeremyauguste <jeremy.augu@gmail.com>
2026-02-14 13:15:17 -0500

    added a little bit of styling

--- react_static_page/src/components/Content.js
+++ react_static_page/src/components/Content.js
@@ -5,9 +5,15 @@ function Content() {
         <div>
             <p>Zhu Yingtai was a girl born to a wealthy family, but she was without an education. She wished to attend the nearby school, but they only accepted boys. Zhu managed to convince her parents to allow her to attend that school disguised as a boy if she was able to do so without getting caught.</p>
 
+            <butterfly>🦋</butterfly>
+
             <p>Zhu managed to use her disguise to get close to a boy she met there named Liang Shanbo. They quickly became close and studied together often. Zhu realized in her heart that she loved Liang. After some time, Zhu needed to leave the school and return home. Before completely departing, she asked if Liang would visit her. After becoming as close as they have, Liang does decide to visit her.</p>
 
+            <butterfly>🦋🦋</butterfly>
+
             <p>Liang found himself at Zhu's home, but when he saw Zhu, he realized that she was actually a woman the entire time. Then and there Liang proposed to Zhu, but Zhu had to tell him that her family had promised her hand in marriage to someone else, which left him heart broken. He attempted to get her family to accept his proposal instead, but Zhu's family declined in order to avoid the dishonorable act of breaking a promise. Liang then fell terribly ill as the day of Zhu's marriage drew near. Eventually, he died.</p>
+
+            <butterfly>🦋♥️🦋</butterfly>
 
             <p>On the way to get married, Zhu found her way to Liang's grave, where she stayed for a little while. Suddenly, Liang's grave opened up! Zhu wasted no time and jumped into it in the hopes of being reunited with the one she married in her heart. Then, her family witnessed two butterflies soar from the grave, signifying Zhu and Liang's love.</p>
         </div>
